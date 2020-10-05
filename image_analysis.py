460b70e36caed940e651a1a849d0ecf3a6eb2824
Ananyapam De <ananyapam7@gmail.com>
2020-10-05 14:43:23 +0530

    commit

--- image_analysis.py
+++ image_analysis.py
@@ -1,7 +1,6 @@
 import os
 import matplotlib.pyplot as plt
-import cv2
 
-img = cv2.imread('../pneumonia-detection/chest_xray/test/PNEUMONIA/person109_bacteria_522.jpeg')
+img = plt.imread('../chest_xray/test/PNEUMONIA/person109_bacteria_527.jpeg')
 plt.imshow(img)
 plt.show()
