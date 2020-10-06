8145050c7f84aa4c3e62902495fa2dbc13bd2bce
Ananyapam De <ananyapam7@gmail.com>
2020-10-06 12:53:44 +0530

    commit

--- image_analysis.py
+++ image_analysis.py
@@ -42,20 +42,6 @@ def extract_properties(image_path):
     
     im = imageio.imread(image_path)
     height, width = im.shape
-    
-    image_path_details = (image_path.split('/')[-1]).split('_')[0]
-    
-    match = re.match(r"([a-z]+)([0-9]+)", image_path_details, re.I)
-    
-    if match:
-        items = match.groups()
-    
-    
-    PERSON_NUMBER = items[1]
-    
-    IMAGE_NUMBER = (image_path.split('/')[-1]).split('_')[-1].split('.')[0]
-
-
     br_med = np.median(im)
     br_std = np.std(im)
     
@@ -70,15 +56,12 @@ def extract_properties(image_path):
     elif "bacteria" in image_path:
         TYPE = "BACTERIAL PNEUMONIA"
     
-    return TYPE, height, width, br_med, br_std, PERSON_NUMBER, IMAGE_NUMBER
-
-print (extract_properties("../chest_xray/test/PNEUMONIA/person1_virus_8.jpeg"))
+    return TYPE, height, width, br_med, br_std
 
 #Viewing the images
 fig = plt.figure(figsize=(15, 10))
 columns = 4; rows = 2
 for i in range(1, columns*rows +1):
-
     img = cv2.imread(images[i])
     img = cv2.resize(img, (128, 128))
     fig.add_subplot(rows, columns, i)
@@ -90,6 +73,7 @@ for i in range(1, columns*rows +1):
 fig=plt.figure(figsize=(15, 10))
 columns = 4; rows = 2
 for i in range(1, columns*rows +1):
+
     img = cv2.imread(images[i])
     img = cv2.resize(img, (512, 512))
     img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
